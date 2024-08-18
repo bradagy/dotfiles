487eba655038e65d5b165c2eef8a7d27869abc8c
bradagy <engineering0x22@gmail.com>
2024-08-18 16:46:15 -0400

    Add information for tmux config file

--- scripts/dotfiles_config.sh
+++ scripts/dotfiles_config.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
-
 # Configuration for dotfiles setup
 DOTFILES_DIR="$HOME/dotfiles"
-FILES=(.bash_profile .vimrc .tmux.conf .gitconfig .ideavimrc .bashrc)
+FILES=(.bash_profile .vimrc .gitconfig .ideavimrc .bashrc)
+TMUX_CONF_DIR=".tmux"
+TMUX_CONF_FILE="tmux.conf"
 REPO_URL="https://github.com/bradagy/dotfiles.git"
